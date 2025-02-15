e9c6d13888104ada3d7accc0948dbc7ce526eee5
ABGACHE <143274169+abgache@users.noreply.github.com>
2025-02-15 12:04:00 +0100

    Update router.php

--- router.php
+++ router.php
@@ -3,7 +3,7 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $file = __DIR__ . $uri;
 
 if (is_file($file)) {
-    return false; // Servir le fichier directement
+    return false;
 } else {
     // Si le fichier n'existe pas, renvoyer vers 404.html
     include __DIR__ . '/404.html';
